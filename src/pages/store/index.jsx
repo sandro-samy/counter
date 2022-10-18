4b02b4e70cf4666aac24dfa853234ec71d7cd38f
sandro <sandrosamy8@gmail.com>
2022-10-18 20:19:06 +0200

    fix "grid"

--- src/pages/store/index.jsx
+++ src/pages/store/index.jsx
@@ -6,7 +6,7 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useDispatch } from "react-redux";
 import { addItem } from "../../store/cartSlice";
 import { truncate } from "javascript-functions";
-
+import styles from "./store.module.css";
 const Store = () => {
   const [products, setProducts] = React.useState([]);
 
@@ -20,37 +20,36 @@ const Store = () => {
   const dispatch = useDispatch();
   return (
     <div className="container p-5">
-      <div className="row gy-5  ">
+      <div className="row gy-5">
         {products &&
           products.map((product) => (
-            <div
-              className="card shadow-sm  col p-3 rounded-4 "
-              key={product.id}
-            >
-              <div style={{ minHeight: "500px" }}>
-                <img src={product.image} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title mb-3">
-                    {truncate(product.title, 20)}
-                  </h5>
-                  <div className="card-text">
-                    <p className="mb-2">{product.category}</p>
-                    <p>{product.price} EGP</p>
+            <div className="col-lg-4 col-md-6 col-sm-12 " key={product.id}>
+              <div className="card rounded-4 shadow-sm p-3">
+                <div className={styles.cardCaption}>
+                  <img src={product.image} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title mb-3">
+                      {truncate(product.title, 20)}
+                    </h5>
+                    <div className="card-text">
+                      <p className="mb-2">{product.category}</p>
+                      <p>{product.price} EGP</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="d-flex flex-column ">
-                <Link
-                  to={`product/${product.id}`}
-                  className="btn btn-primary w-100 mb-2"
-                >
-                  more details
-                </Link>
-                <Button
-                  action={() => dispatch(addItem(product))}
-                  text={<AiOutlineShoppingCart className="fs-3" />}
-                  className="btn-success w-100 mx-0"
-                ></Button>
+                <div className="d-flex flex-column ">
+                  <Link
+                    to={`product/${product.id}`}
+                    className="btn btn-primary w-100 mb-2"
+                  >
+                    more details
+                  </Link>
+                  <Button
+                    action={() => dispatch(addItem(product))}
+                    text={<AiOutlineShoppingCart className="fs-3" />}
+                    className="btn-success w-100 mx-0"
+                  ></Button>
+                </div>
               </div>
             </div>
           ))}
